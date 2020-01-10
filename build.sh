8d2d0643a11bc26a78d2e72723970ff9e5391f79
Heberti Almeida <hebertialmeida@gmail.com>
2020-01-10 14:48:29 -0500

    Fix build file identation

--- build.sh
+++ build.sh
@@ -13,21 +13,22 @@ time xcodebuild clean test \
     | tee build.log \
     | xcpretty &&
 rm -rf $DERIVED_DATA &&
-    time xcodebuild clean test \
+time xcodebuild clean test \
     -scheme ConsistencyManager \
     -derivedDataPath $DERIVED_DATA \
-    -destination 'platform=tvOS Simulator,name=Apple TV,OS=13.3' \
-    | tee build.log \
-    | xcpretty &&
-rm -rf $DERIVED_DATA &&
-    time xcodebuild clean test \
-    -scheme ConsistencyManager \
     -sdk iphonesimulator \
     -derivedDataPath $DERIVED_DATA \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3.1' \
     -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.4' \
     -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4' \
     -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3' \
+    | tee build.log \
+    | xcpretty &&
+rm -rf $DERIVED_DATA &&
+    time xcodebuild clean test \
+    -scheme ConsistencyManager \
+    -sdk appletvsimulator \
+    -destination 'platform=tvOS Simulator,name=Apple TV,OS=13.3' \
     | tee build.log \
     | xcpretty &&
 cat build.log
