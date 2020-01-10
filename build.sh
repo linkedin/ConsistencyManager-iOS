1b7c9b67d3c1a710c57d7751f106d43661d0a6a7
Heberti Almeida <hebertialmeida@gmail.com>
2020-01-10 14:40:44 -0500

    One more fix for derived data path

--- build.sh
+++ build.sh
@@ -9,22 +9,25 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -scheme ConsistencyManager \
     -sdk macosx \
+    -derivedDataPath $DERIVED_DATA \
     | tee build.log \
     | xcpretty &&
 rm -rf $DERIVED_DATA &&
-time xcodebuild clean test \
+    time xcodebuild clean test \
     -scheme ConsistencyManager \
+    -derivedDataPath $DERIVED_DATA \
     -destination 'platform=tvOS Simulator,name=Apple TV,OS=13.3' \
     | tee build.log \
     | xcpretty &&
 rm -rf $DERIVED_DATA &&
-time xcodebuild clean test \
+    time xcodebuild clean test \
     -scheme ConsistencyManager \
     -sdk iphonesimulator \
+    -derivedDataPath $DERIVED_DATA \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3.1' \
     -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.4' \
     -destination 'platform=iOS Simulator,name=iPhone X,OS=12.4' \
     -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3' \
     | tee build.log \
-    | xcpretty
+    | xcpretty &&
 cat build.log
