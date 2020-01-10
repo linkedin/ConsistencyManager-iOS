f5b54d439dd58e3b9727e7271b0f37bcee6df776
Heberti Almeida <hebertialmeida@gmail.com>
2020-01-10 13:49:08 -0500

    Update travis vm and build script to include more devices

--- build.sh
+++ build.sh
@@ -1,39 +1,25 @@
 #!/bin/sh
-# Builds all targets and runs tests.
 
-DERIVED_DATA=${1:-/tmp/ConsistencyManager}
-echo "Derived data location: $DERIVED_DATA";
-
+# macOS
 set -o pipefail &&
-rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
-    -project ConsistencyManager.xcodeproj \
     -scheme ConsistencyManager \
     -sdk macosx \
-    -derivedDataPath $DERIVED_DATA \
-    | tee build.log \
-    | xcpretty &&
-rm -rf $DERIVED_DATA &&
-time xcodebuild clean build \
-    -project ConsistencyManager.xcodeproj \
-    -scheme ConsistencyManager \
-    -sdk iphonesimulator \
-    -derivedDataPath $DERIVED_DATA \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.1' \
-    -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.3' \
-    -destination 'platform=iOS Simulator,name=iPhone XS Max,OS=12.2' \
-    | tee build.log \
-    | xcpretty &&
-rm -rf $DERIVED_DATA &&
-    time xcodebuild clean build \
-    -project ConsistencyManager.xcodeproj \
-    -scheme ConsistencyManager \
-    -sdk appletvsimulator \
-    -derivedDataPath $DERIVED_DATA \
-    -destination 'platform=tvOS Simulator,name=Apple TV,OS=12.2' \
-    | tee build.log \
-    | xcpretty &&
-cat build.log
+| xcpretty
 
+# tvOS
+set -o pipefail &&
+time xcodebuild clean test \
+    -scheme ConsistencyManager \
+    -destination 'platform=tvOS Simulator,name=Apple TV,OS=13.3' \
+| xcpretty
+
+# iOS
+set -o pipefail &&
+time xcodebuild clean test \
+    -scheme ConsistencyManager \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3.1' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.4' \
+    -destination 'platform=iOS Simulator,name=iPhone X,OS=12.2' \
+    -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.3' \
+| xcpretty
